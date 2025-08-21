7a6a6c4d7d876f79d65570a8eb076c8428bae61a
Probug <probug@gmail.com>
2025-08-21 13:11:03 +0900

    Add files via upload

---
+++ TestForm/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvasPanel = new BaseComponent.CanvasPanel();
+            this.propertyGrid = new System.Windows.Forms.PropertyGrid();
+            this.canvasPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.AllowDrop = true;
+            this.canvasPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.canvasPanel.BoldLineColor = System.Drawing.Color.Gray;
+            this.canvasPanel.Controls.Add(this.propertyGrid);
+            this.canvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvasPanel.GridSize = 20;
+            this.canvasPanel.IsMenuLock = false;
+            this.canvasPanel.LightLineColor = System.Drawing.Color.LightGray;
+            this.canvasPanel.Location = new System.Drawing.Point(0, 0);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(1516, 967);
+            this.canvasPanel.TabIndex = 0;
+            this.canvasPanel.TabStop = true;
+            // 
+            // propertyGrid
+            // 
+            this.propertyGrid.Location = new System.Drawing.Point(0, 0);
+            this.propertyGrid.Name = "propertyGrid";
+            this.propertyGrid.Size = new System.Drawing.Size(295, 416);
+            this.propertyGrid.TabIndex = 0;
+            this.propertyGrid.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1516, 967);
+            this.Controls.Add(this.canvasPanel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sample";
+            this.canvasPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BaseComponent.CanvasPanel canvasPanel;
+        private System.Windows.Forms.PropertyGrid propertyGrid;
+    }
+}
+
---
+++ TestForm/Form1.cs
@@ -0,0 +1,130 @@
+﻿using BaseComponent;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        private BasePanel currentSelectedComponent = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            canvasPanel.ComponentSelected += CanvasPanel_ComponentSelected;
+
+            //특정 컴포넌트 갯수 재한 예제
+            canvasPanel.SetComponentLimit("ImagePlugin", 20);
+            canvasPanel.SetComponentLimit("SamplePlugin", 20);
+        }
+
+        private void CanvasPanel_ComponentSelected(BasePanel obj)
+        {
+            if (obj != null)
+            {
+                foreach (Control ctrl in canvasPanel.Controls)
+                {
+                    if (ctrl is BasePanel bp)
+                        bp.SetSelected(false);
+                }
+
+                if (obj is BasePanel selected)
+                {
+                    selected.SetSelected(true);
+
+                    // 기존 이벤트 제거
+                    if (currentSelectedComponent != null)
+                    {
+                        currentSelectedComponent.Move -= SelectedComponent_Moved;
+                        currentSelectedComponent.SizeChanged -= SelectedComponent_Resized;
+                    }
+
+
+                    // 새 선택된 컴포넌트 등록 + Move 이벤트 연결
+                    currentSelectedComponent = selected;
+                    currentSelectedComponent.Move += SelectedComponent_Moved;
+                    currentSelectedComponent.SizeChanged += SelectedComponent_Resized;
+
+                    // 기준 위치: CanvasPanel 내에서의 컴포넌트 위치
+                    UpdatePropertyGridPosition(selected);
+
+                    // 적용
+                    propertyGrid.BringToFront();
+                    propertyGrid.SelectedObject = obj;
+                    propertyGrid.Visible = canvasPanel.isPropertyGridVisible;
+                }
+            }
+            else
+            {
+                // 선택 해제 시 숨김 + 이벤트 해제
+                if (currentSelectedComponent != null)
+                {
+                    currentSelectedComponent.Move -= SelectedComponent_Moved;
+                    currentSelectedComponent.SizeChanged -= SelectedComponent_Resized;
+                }
+
+                // 선택 해제 시 숨김
+                currentSelectedComponent = null;
+                propertyGrid.Visible = false;
+                propertyGrid.SelectedObject = null;
+            }
+        }
+
+
+        private void SelectedComponent_Resized(object sender, EventArgs e)
+        {
+            if (sender is BasePanel panel && propertyGrid.Visible)
+            {
+                UpdatePropertyGridPosition(panel);
+            }
+        }
+
+        private void SelectedComponent_Moved(object sender, EventArgs e)
+        {
+            if (propertyGrid.Visible && sender is BasePanel panel)
+            {
+                UpdatePropertyGridPosition(panel);
+            }
+        }
+
+        private void UpdatePropertyGridPosition(BasePanel selected)
+        {
+            if (!canvasPanel.isPropertyGridVisible) return;
+
+            int padding = 6;
+
+            Size gridSize = propertyGrid.PreferredSize;
+            Size panelSize = canvasPanel.ClientSize;
+
+            Point compPos = canvasPanel.PointToClient(selected.PointToScreen(Point.Empty));
+            int compRight = compPos.X + selected.Width;
+            int compTop = compPos.Y;
+
+            int desiredX = compRight + padding;
+            int desiredY = compTop;
+
+            // 우측 경계
+            if (desiredX + gridSize.Width > panelSize.Width)
+                desiredX = panelSize.Width - gridSize.Width - padding;
+
+            if (desiredX < padding)
+                desiredX = padding;
+
+            // 아래쪽 경계
+            if (desiredY + gridSize.Height > panelSize.Height)
+                desiredY = panelSize.Height - gridSize.Height - padding;
+
+            if (desiredY < padding)
+                desiredY = padding;
+
+            propertyGrid.Location = new Point(desiredX, desiredY);
+        }
+    }
+}
